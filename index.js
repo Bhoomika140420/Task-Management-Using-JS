36418d3b5e66e68c88bef5336302a0d2dd485f83
Bhoomika P <82462226+Bhoomika140420@users.noreply.github.com>
2023-10-09 12:06:08 +0530

    Update index.js
    edit task part added

--- index.js
+++ index.js
@@ -74,13 +74,58 @@ const deleteTask = (e) => {
     window.location.reload();
 }
 
-const editTask = (e) => {
-    const targetID = e.getAttribute("name");
-    console.log(e);
-    console.log(e.parentNode);    
-    console.log(e.parentNode.parentNode.parentNode);
-    console.log(e.parentNode.parentNode.parentNode.childNodes[5].childNodes[1]);
-    // console.log(globalTaskData);
-    saveToLocalStorage(); //function call
-    // window.location.reload();
+//edit function
+const saveEditTask =(e) =>{
+    const targetId = e.getAttribute("name")
+    const taskTitle = e.parentNode.parentNode.childNodes[5].childNodes[1]
+    const taskDescription = e.parentNode.parentNode.childNodes[5].childNodes[3]
+    const taskType = e.parentNode.parentNode.childNodes[5].childNodes[5]
+    const updateData = {
+        taskTitle: taskTitle.innerHTML,
+        taskDescription: taskDescription.innerHTML,
+        taskType: taskType.innerHTML,
+    }
+    let globalTaskCopy = globalTaskData
+    globalTaskCopy = globalTaskCopy.map((task)=>
+        task.id === targetId
+        ? {
+            id: task.id,
+            url: task.url,
+            title: updateData.taskTitle,
+            description: updateData.taskDescription,
+            type: updateData.taskType
+         }
+        : task
+
+    )
+
+   globalTaskData = globalTaskCopy
+   saveToLocalStorage()
+    
+
+    e.parentNode.parentNode.childNodes[5].childNodes[1].setAttribute("contenteditable","false")
+    e.parentNode.parentNode.childNodes[5].childNodes[3].setAttribute("contenteditable","false")
+    e.parentNode.parentNode.childNodes[5].childNodes[5].setAttribute("contenteditable","false")
+    e.parentNode.parentNode.childNodes[7].childNodes[1].innerHTML = "OPEN TASK"
+    e.parentNode.parentNode.childNodes[7].childNodes[1].setAttribute("onclick","openModal(this)")
+    e.parentNode.parentNode.childNodes[7].childNodes[1].setAttribute("data-bs-toggle","modal");
+    e.parentNode.parentNode.childNodes[7].childNodes[1].setAttribute("data-bs-target","#openTask");
+    
+}
+
+const openModal =(e)=>{
+    const targetId = e.getAttribute("name")
+    const getTask = globalTaskData.filter((e) => e.id===targetId)
+    modalContents.innerHTML =openModalDispaly(getTask[0])
+}
+
+const openModalDispaly=({id, url, title, type, description})=>{
+const date = new Date(parseInt(id));
+console.log(date)
+return `
+<img class="card-img-top mb-3 rounded-lg"src=${url} alt="Task image">
+<strong class="text-sm text-muted">Created on ${date.toDateString()}</strong>
+ <h3 class="card-title">${title}</h5>
+ <p class="card-text">${description}</p>
+ <span class="badge bg-primary">${type}</span>`
 }
